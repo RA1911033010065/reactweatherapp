1c558a3c6a36f1c6b149d25aa4ca74b18b16c384
Mohit Gupta <mg6489@srmist.edu.in>
2021-12-23 20:06:31 +0530

    search function implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React, { useState } from 'react';
 
 const api ={
   key:"572bcaf1f873c2e9709e18e742cd9049",
   base:"https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const findweather = event => {
+    if (event.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -25,6 +39,9 @@ function App() {
     type = "text"
     className="search-bar"
     placeholder="City Name...."
+    onChange={e => setQuery(e.target.value)}
+    value={query}
+    onKeyPress={findweather}
     
     />
    </div>
