dfa617230a021a9445f7569ac7d8d841fd127767
Mohit Gupta <mg6489@srmist.edu.in>
2021-12-23 19:03:17 +0530

    datebuilder added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ const api ={
   base:"https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
    <div className="app-warm">
      <main>
